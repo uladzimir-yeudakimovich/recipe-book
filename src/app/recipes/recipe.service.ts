f117e2030613e0715dfa1815b1a538ea2e862569
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-02-25 11:24:09 +0300

    programmatic navigation to the Edit Page

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,16 +10,16 @@ export class RecipeService {
   
 	private recipes: Recipe[] = [
 		new Recipe(
-      'A Tast Recipe', 
-      'This is simply a test', 
+      'Tasty Schnitzel', 
+      'A super-tasty Schnitzel - just awesome!', 
       'https://www.weightwatchers.co.uk/images/2057/dynamic/foodandrecipes/2013/10/SteakChipsMustardMayo_xl.jpg', 
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
       ]),
     new Recipe(
-      'Another Tast Recipe', 
-      'This is simply a test', 
+      'Big Fat Burger', 
+      'What else you need to say?', 
       'https://natashaskitchen.com/wp-content/uploads/2019/04/Best-Burger-4-500x500.jpg', 
       [
         new Ingredient('Buns', 2),
