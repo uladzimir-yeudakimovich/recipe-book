5c71232281aba6b2fdafe45099039fbde14c5d13
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-03-06 00:08:31 +0300

    Modifying Requests in Interceptors

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -1,10 +1,18 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 
+import { AuthService } from '../auth/auth.service';
+
+@Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Intercepted', req);
-    return next.handle(req);
+    // const copiedReq = req.clone({headers: req.headers.append('', '')});
+    const copiedReq = req.clone({params: req.params.append('auth', this.authService.getToken())});
+    return next.handle(copiedReq);
     // return null;
   }
 }
