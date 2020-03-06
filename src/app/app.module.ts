b2049eff0c8bdaf9ad0d599d7364496d06c0d3ed
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-03-06 22:53:58 +0300

    Adjusting the App Module Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { SharedModule } from './shared/shared.module';
 import { ShoppindListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
-import { shoppingListReducer } from './shopping-list/store/shopping-list.reducers';
+import { reducers } from './store/app.reducers';
 
 @NgModule({
   declarations: [
@@ -23,7 +23,7 @@ import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer
     ShoppindListModule,
     AuthModule,
     CoreModule,
-    StoreModule.forRoot({shoppingList: shoppingListReducer})
+    StoreModule.forRoot(reducers)
   ],
   bootstrap: [AppComponent]
 })
